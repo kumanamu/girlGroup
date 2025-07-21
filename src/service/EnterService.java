1144a52cdbe620d983b7684a013e36131fb84ee8
kumanamu <serachstock1@gmail.com>
2025-07-21 10:04:39 +0900

    걸그룹 init

---
+++ src/service/EnterService.java
@@ -0,0 +1,26 @@
+package service;
+
+import dto.Entertainment;
+import dto.GirlGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnterService {
+
+    private List<Entertainment> entertainments = new ArrayList<>();
+    private List<GirlGroup> groups = new ArrayList<>();
+
+    private List<Entertainment>  getAllEntertainment() {
+
+        System.out.println("[EnterService.getAllEntertainment]");
+        return null;
+
+
+    }
+
+    public List<GirlGroup> getGroups(int enterId) {
+        System.out.println("EnterService.getGroups");
+        return null;
+    }
+    }
---
+++ src/view/ViewProcess.java
@@ -0,0 +1,16 @@
+package view;
+
+public class ViewProcess {
+    public void getEntertaimnet() {
+        System.out.println("소속사 정보 출력");
+    }
+
+
+    public void findByEnterID(int num) {
+        System.out.println("그룹정보");
+
+
+    }
+
+
+}
