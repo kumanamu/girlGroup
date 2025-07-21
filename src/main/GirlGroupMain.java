c2d108b36450c73d13bef7fe609a3480faf7cebd
kumanamu <serachstock1@gmail.com>
2025-07-21 11:01:56 +0900

    최종작업

--- src/main/GirlGroupMain.java
+++ src/main/GirlGroupMain.java
@@ -6,31 +6,26 @@ import java.util.Scanner;
 
 public class GirlGroupMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         ViewProcess viewProcess = new ViewProcess();
 
-
+        Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("소속사 정보");
-            //service 에서 소속사 정보 가져오기
-            //view 호출하기
-            viewProcess.getEntertaimnet();
-
-            System.out.println("소속 그룹 정보를 보려면 id를 입력하세요, 종료 : 0");
+            // 서비스에서 소속사 정보 가져오기
+            // 뷰 기능 호출하기
+            viewProcess.getEntertainment();
+            System.out.println("소속 그룹 정보를 보려면 ID 입력, 종료 : 0");
             int num = sc.nextInt();
-
             if (num == 0) {
                 return;
-
             } else {
-                //서비스에서 해당 소속사의 그룹 정보 가져오기
-                //뷰를 통해서 기능 호출하기
+                // 서비스에서 해당 소속사의 그룹 정보 가져오기
+                // 뷰 기능 호출하기
                 viewProcess.findByEnterID(num);
+                break;
 
             }
-
-
         }
     }
 }
